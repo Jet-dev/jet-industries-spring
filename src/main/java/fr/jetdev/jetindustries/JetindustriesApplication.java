91d272e610e2eb0e6f657f2acd17e9012db67ef2
William Leemans <will.leemans@gmail.com>
2022-11-03 17:10:50 +0100

    🎉 Init Spring server #2

---
+++ src/main/java/fr/jetdev/jetindustries/JetindustriesApplication.java
@@ -0,0 +1,13 @@
+package fr.jetdev.jetindustries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetindustriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetindustriesApplication.class, args);
+	}
+
+}
